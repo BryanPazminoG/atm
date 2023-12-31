235157b4e72e4c9ab2d0733bca5d4b17f00bf4a4
GisseTefy <gisselle.estefania1997@gmail.com>
2023-12-30 19:43:44 -0500

    resolucion conflictos

--- src/app/Paginas/transacciones-con-tarjeta/transacciones-con-tarjeta-routing.module.ts
+++ src/app/Paginas/transacciones-con-tarjeta/transacciones-con-tarjeta-routing.module.ts
@@ -1,23 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SelecciontComponent } from './selecciont/selecciont.component';
-<<<<<<< HEAD
 import { ComprobanteretiroctComponent } from './comprobanteretiroct/comprobanteretiroct.component';
 
 
 
-const routes: Routes = [
-  { path: 'selecciont', component: SelecciontComponent},
-  { path: 'retirosconfirmct', component: ComprobanteretiroctComponent},
-=======
-import { DepositoconfirmctComponent } from './depositoconfirmct/depositoconfirmct.component';
-import { CantidadretiroComponent } from './cantidadretiro/cantidadretiro.component';
 
 const routes: Routes = [
+
   { path: 'selecciont', component: SelecciontComponent},
-  { path: 'retiroconfirm', component: DepositoconfirmctComponent},
-  { path: 'cantidadretiro', component: CantidadretiroComponent}
->>>>>>> 37ad5f49a3fb56cb6de8baa0ed977061d9c3f802
+  { path: 'retiroconfirm', component: ComprobanteretiroctComponent},
 
 ];
 
--- src/app/Paginas/transacciones-con-tarjeta/transacciones-con-tarjeta.module.ts
+++ src/app/Paginas/transacciones-con-tarjeta/transacciones-con-tarjeta.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InsercionTarjetaComponent } from '../../Bloques/insercion-tarjeta/insercion-tarjeta.component';
 import { TransaccionesConTarjetaRoutingModule } from './transacciones-con-tarjeta-routing.module';
-import { CantidadretiroComponent } from './cantidadretiro/cantidadretiro.component';
 
 // PrimeNG
 import { ButtonModule } from 'primeng/button';
@@ -15,11 +14,8 @@ import { ComprobanteretiroctComponent } from './comprobanteretiroct/comprobanter
 @NgModule({
   declarations: [
     InsercionTarjetaComponent,
-<<<<<<< HEAD
     ComprobanteretiroctComponent
-=======
-    CantidadretiroComponent
->>>>>>> 37ad5f49a3fb56cb6de8baa0ed977061d9c3f802
+
   ],
   imports: [
     CommonModule,
